660c17cb3cb16008da908a11768eed9ac96ec073
Gustav Rasmussen <gcr84@hotmail.com>
2019-04-30 19:57:12 +0200

    Add file

---
+++ Scripts/Maintenance/request_data.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def get_data():
+    URL = "https://github.com/Gustav-Rasmussen/N-Body-Simulations/tree/master/textfiles/StableStructures"
+    r = requests.get(URL)
+    if r.status_code == 200:
+        return r.json()
+    return None
+
+
+if __name__ == '__main__':
+    get_data()
