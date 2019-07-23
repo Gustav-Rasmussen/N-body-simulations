1ee78b8b11c23acdcd4adb4bb098cea2bccf835c
Gustav Rasmussen <gcr84@hotmail.com>
2019-07-23 22:17:34 +0200

    Update script

--- Scripts/Maintenance/rename.py
+++ Scripts/Maintenance/rename.py
@@ -20,30 +20,11 @@ def change_dir(destination):
 # txtFilePath = Path.cwd() / 'Desktop/GperturbNew/StableStructures/textFiles'
 # txtFilePath = Path.cwd().parent / 'textFiles'
 
-sims = ['A',
-        'B',
-        'CS1',
-        'CS2',
-        'CS3',
-        'CS4',
-        'CS5',
-        'CS6',
-        'D2',
-        'DS1',
-        'E',
-        'MartinICandFinalEddandOM',
-        'softD2'
-        ]
+sims = ['A', 'B', 'CS1', 'CS2', 'CS3', 'CS4',
+        'CS5', 'CS6', 'D2', 'DS1', 'E', 'MartinICandFinalEddandOM', 'softD2']
 
-modules = ["readVDF_2_new",
-           "fileLsts",
-           "readVDF2",
-           "VDFnetwork",
-           "readVDF",
-           "TimeEvolutionGammaKappaBeta",
-           "Sigma",
-           "readVDF_2_new"
-           ]
+modules = ["readVDF_2_new", "fileLsts", "readVDF2", "VDFnetwork", "readVDF",
+           "TimeEvolutionGammaKappaBeta", "Sigma", "readVDF_2_new"]
 
 # folders = [f'{txtFilePath}/{i}' for i in sims]
 
@@ -52,11 +33,82 @@ for i in sims:
     vars()[i] = f'{txtFilePath}/{i}'
 '''
 
+file_name = ''
+line = ''
+# readfile = f'{modules[2]}.txt'
+# writefile = f'{modules[2]}_copy.txt'
+readfile = 'testfile.txt'
+writefile = 'testfile_copy.txt'
 
-def file_replace(from: str, to: str, in_str=from: str):
+filename_dict = {'Hernquist': 'HQ',
+                 'Hq': 'HQ',
+                 'Osipkov': 'O',
+                 'Merritt': 'M',
+                 'phi': 'Phi',
+                 'theta': 'Theta',
+                 'Gamma_-1.50': 'Gamma_-1.5',
+                 'Gamma_-2.00': 'Gamma_-2.0',
+                 'Gamma_-2.50': 'Gamma_-2.5',
+                 'Gamma_-3.00': 'Gamma_-3.0'
+                 }
+
+line_dict = {'Eddington': 'Edd',
+             'perturbation': 'Pert',
+             'Hernquist': 'HQ',
+             'Hq': 'HQ',
+             'Osipkov_Merritt': 'OM',
+             'Osipkov': 'O',
+             'Merritt': 'M',
+             'figure_path': 'figurePath',
+             'datalist_innerbin': 'bin1',
+             'datalist_first_middlebin': 'bin2',
+             'datalist_second_middlebin': 'bin3',
+             'datalist_outerbin': 'bin4',
+             'list_of_files_': 'FileLst',
+             'innerbin_': 'bin1',
+             'first_middlebin_': 'bin2',
+             'second_middlebin_': 'bin3',
+             'outerbin_': 'bin4',
+             'radial_bins': 'RBins',
+             'R_limit': 'Rlim',
+             'Almost_Final': 'SecondLast',
+             'phi': 'Phi',
+             'theta': 'Theta',
+             '_new_R_middle_': '',
+             'large_R_middle_19_95_': 'bin5',
+             'large_R_middle_31_62_': 'bin6',
+             '_large_R_middle_': '',
+             '\'/Users/gustav.c.rasmussen': 'os.getcwd() + \'',
+             'Red': 'r',
+             'Green': 'g',
+             'Black': 'k',
+             'Blue': 'b',
+             '_average_inside_bin_': '_BinAvg_',
+             '_avg_in_bin_': '_BinAvg_',
+             'average_': 'avg_',
+             'color=': '',
+             'color =': '',
+             ' )': ')',
+             'data, label': 'data, _'
+             }
+
+
+def find_replace_multi_ordered(string, dictionary):
+    # sort keys by length, in reverse order
+    for item in sorted(dictionary.keys(), key=len, reverse=True):
+        string = re.sub(item, dictionary[item], string)
+    return string
+
+
+s = "HEREABCISABCACSAMPLEABCSTRING"
+d = {"C": "-", "ABC": "-", "CSAMPLEABC": "-:)-"}
+# find_replace_multi_ordered(s, d)
+
+
+def file_replace(From: str, to: str, in_str: str = ''):
     """."""
     if in_str in file_name:
-        return file_name.replace(from, to)
+        return file_name.replace(From, to)
     return file_name
 
 
@@ -66,34 +118,24 @@ def rename_files(files):
             continue
         file_name, file_ext = os.path.splitext(f)
         # print(file_name)
-
-        file_name = file_replace('Hernquist', 'HQ')
-        file_name = file_replace('Hq', 'HQ')
-        file_name = file_replace('Osipkov', 'O')
-        file_name = file_replace('Merritt', 'M')
+        # for k, v in filename_dict.items():
+        #     file_name = file_replace(k, v)
+        file_name = find_replace_multi_ordered(file_name, filename_dict)
         file_name = file_replace('rad', 'R', 'Sigmarad')
         file_name = file_replace('tan', 'T', 'Sigmatan')
-        file_name = file_replace('phi', 'Phi')
-        file_name = file_replace('theta', 'Theta')
-        file_name = file_replace('Gamma_-1.50', 'Gamma_-1.5')
-        file_name = file_replace('Gamma_-2.00', 'Gamma_-2.0')
-        file_name = file_replace('Gamma_-2.50', 'Gamma_-2.5')
-        file_name = file_replace('Gamma_-3.00', 'Gamma_-3.0')
-
         if ('LargeRMiddle' in file_name) and ('RMiddle' in file_name):
             file_name = file_name.replace('LargeRMiddle', '')
             file_name = file_name.replace('RMiddle', 'LargeRMiddle')
-
         new_name = regexUpper.regex_sub_upper(file_name)
         new_file = f'{new_name}{file_ext}'
         # print(f'{new_file}')
         os.rename(f, new_file)
 
 
-def line_replace(from: str, to: str, in_str=from: str):
+def line_replace(From: str, to: str, in_str: str = ''):
     """."""
     if in_str in line:
-        return line.replace(from, to)
+        return line.replace(From, to)
     return line
 
 
@@ -101,50 +143,12 @@ def replace_lines():
     with open(readfile, "r") as rf:
         with open(writefile, "w") as wf:
             for line in rf:
-                line = line_replace('Eddington', 'Edd')
-                line = line_replace('perturbation', 'Pert')
-                line = line_replace('Hernquist', 'HQ')
-                line = line_replace('Hq', 'HQ')
-                line = line_replace('Osipkov_Merritt', 'OM')
-                line = line_replace('Osipkov', 'O')
-                line = line_replace('Merritt', 'M')
-                line = line_replace('figure_path', 'figurePath')
-                line = line_replace('datalist_innerbin', 'bin1')
-                line = line_replace('datalist_first_middlebin', 'bin2')
-                line = line_replace('datalist_second_middlebin', 'bin3')
-                line = line_replace('datalist_outerbin', 'bin4')
-                line = line_replace('list_of_files_', 'FileLst')
-                line = line_replace('innerbin_', 'bin1')
-                line = line_replace('first_middlebin_', 'bin2')
-                line = line_replace('second_middlebin_', 'bin3')
-                line = line_replace('outerbin_', 'bin4')
-                line = line_replace('radial_bins', 'RBins')
-                line = line_replace('R_limit', 'Rlim')
-                line = line_replace('Almost_Final', 'SecondLast')
-                line = line_replace('phi', 'Phi')
-                line = line_replace('theta', 'Theta')
-                line = line_replace('_new_R_middle_', '')
-                line = line_replace('large_R_middle_19_95_', 'bin5')
-                line = line_replace('large_R_middle_31_62_', 'bin6')
-                line = line_replace('_large_R_middle_', '')
-                line = line_replace('\'/Users/gustav.c.rasmussen',
-                                    'os.getcwd() + \'')
-                # Use plt color codes.
-                line = line_replace('Red', 'r')
-                line = line_replace('Green', 'g')
-                line = line_replace('Black', 'k')
-                line = line_replace('Blue', 'b')
-                line = line_replace('yan', '', 'cyan')
-                line = line_replace('agenta', '', 'magenta')
-                line = line_replace('hite', '', 'white')
-                line = line_replace('_average_inside_bin_', '_BinAvg_')
-                line = line_replace('_avg_in_bin_', '_BinAvg_')
-                line = line_replace('average_', 'avg_')
-                line = line_replace('color=', '')
-                line = line_replace('color =', '')
-                line = line_replace(' )', ')')
-                line = line_replace('data, label', 'data, _')
-                line = line_replace('', '')
+                # for k, v in line_dict.items():
+                #     line = line_replace(k, v)
+                line = find_replace_multi_ordered(line, line_dict)
+                # line = line_replace('yan', '', 'cyan')
+                # line = line_replace('agenta', '', 'magenta')
+                # line = line_replace('hite', '', 'white')
                 if any(x in line for x in ['Sigmarad', 'sigmarad']):
                     line = line.replace('rad', 'R')
                 if any(x in line for x in ['Sigmatan', 'sigmatan']):
@@ -160,8 +164,6 @@ def replace_lines():
 
 
 def main():
-    readfile = f'{modules[2]}.txt'
-    writefile = f'{modules[2]}_copy.txt'
     # print(readfile, writefile)
     replace_lines()
     '''
