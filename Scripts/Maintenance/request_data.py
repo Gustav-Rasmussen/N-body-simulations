14f8d1a7bbe41df0d5b5a86c7334117af3c300a9
Gustav Rasmussen <gcr84@hotmail.com>
2019-08-17 19:30:39 +0200

    Update script

--- Scripts/Maintenance/request_data.py
+++ Scripts/Maintenance/request_data.py
@@ -1,19 +1,22 @@
 import requests
+import pandas as pd
 
 
 def get_data(file):
-    github_usr = "https://github.com/Gustav-Rasmussen/"
-    repo = "N-Body-Simulations/tree/master/"
+    github_usr = "https://raw.githubusercontent.com/Gustav-Rasmussen/"
+    repo = "N-Body-Simulations/master/"
     datafiles = "textfiles/StableStructures/"
-
     URL = github_usr + repo + datafiles + file
-
     r = requests.get(URL)
     if r.status_code == 200:
-        return r.json()
+        rr = r.text
+        ff = [i.split(" ") for i in rr.split("\n")[:-1]]
+        df = pd.DataFrame(ff).astype(float).values
+        return df
     return None
 
 
 if __name__ == '__main__':
     file = "A/AHQ10000G1.0_0_000.txt"
-    get_data(file)
+    df = get_data(file)
+    print(df)
