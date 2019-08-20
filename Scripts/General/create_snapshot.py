871b41de7e76ba5409abcc5bc197431273b239dc
Gustav Rasmussen <gcr84@hotmail.com>
2019-08-20 14:18:25 +0200

    Update script

--- Scripts/General/create_snapshot.py
+++ Scripts/General/create_snapshot.py
@@ -3,8 +3,8 @@
 import h5py
 import numpy as np
 
-OldSnapfile = h5py.File('SNAPSHOT FILENAME', 'r')
-NewSnapfile = h5py.File('NEW SNAPSHOT FILENAME', 'w')
+OldSnapfile = h5py.File('SNAPSHOT_FILENAME', 'r')
+NewSnapfile = h5py.File('NEW_SNAPSHOT_FILENAME', 'w')
 
 # Copy header to new snapshot:
 OldSnapfile.copy('/Header', NewSnapfile, '/Header')
@@ -16,7 +16,7 @@ NewSnapfile['PartType1/Velocities'] = Vel
 
 # Set Ndm:
 Ndm = NewSnapfile['PartType1/Masses'].shape[0]
-Narray=np.array([0, Ndm, 0, 0, 0, 0], dtype=np.int32)
+Narray = np.array([0, Ndm, 0, 0, 0, 0], dtype=np.int32)
 NewSnapfile['Header'].attrs.modify('NumPart_ThisFile', Narray)
 NewSnapfile['Header'].attrs.modify('NumPart_Total', Narray)
 
