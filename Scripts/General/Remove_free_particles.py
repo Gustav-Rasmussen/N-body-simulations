aa3ec6b4ae9fbe498636887efd22f068dd5ea1d6
Gustav Rasmussen <gcr84@hotmail.com>
2019-08-20 14:01:49 +0200

    Update script

--- Scripts/General/Remove_free_particles.py
+++ Scripts/General/Remove_free_particles.py
@@ -16,13 +16,16 @@ OldSnaps_list = ['G_HQ_1000000_B/output/Hernquist10000_G1.0_198_093',
 
 OldSnapfile = h5py.File(path + OldSnaps_list[0] + '.hdf5', 'r')
 
-# NewSnapfile = h5py.File('B_G1.0_198_093_no_free_par.hdf5', 'w')
-# NewSnapfile = h5py.File('B_G1.0_199_093_rfp.hdf5', 'w')
-# NewSnapfile = h5py.File('C4_G1.0_48_093_rfp.hdf5', 'w')
-# NewSnapfile = h5py.File('C5_G1.0_48_093_rfp.hdf5', 'w')
-# NewSnapfile = h5py.File('C6_G1.0_48_093_rfp.hdf5', 'w')
-# NewSnapfile = h5py.File('D1_G1.0_49_093_rfp.hdf5', 'w')
-# NewSnapfile = h5py.File('D2_G1.0_49_093_rfp.hdf5', 'w')
+NewSnaps_list = ['B_G1.0_198_093_no_free_par',
+                 'B_G1.0_199_093_rfp',
+                 'C4_G1.0_48_093_rfp',
+                 'C5_G1.0_48_093_rfp',
+                 'C6_G1.0_48_093_rfp',
+                 'D1_G1.0_49_093_rfp',
+                 'D2_G1.0_49_093_rfp
+                ]
+
+NewSnapfile = h5py.File(NewSnaps_list[0] + '.hdf5', 'w')
 
 # copy header to new snapshot:
 OldSnapfile.copy('/Header', NewSnapfile, '/Header')
@@ -35,9 +38,9 @@ V = OldSnapfile['PartType1/Potential'].value
 x = Pos[:, 0]
 y = Pos[:, 1]
 z = Pos[:, 2]
-vx = Vel[:,0]
-vy = Vel[:,1]
-vz = Vel[:,2]
+vx = Vel[:, 0]
+vy = Vel[:, 1]
+vz = Vel[:, 2]
 v = (vx ** 2 + vy ** 2 + vz ** 2) ** .5  # Speed
 E_tot = V + .5 * v ** 2
 GoodIDs = np.where(E_tot <= 0.)  # Bound particles
