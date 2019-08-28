dfec1954323409764ffb9ece80063a577a2d5085
Gustav Rasmussen <gcr84@hotmail.com>
2019-08-28 22:46:57 +0200

    Update script

--- Scripts/VDF_LOS.py
+++ Scripts/VDF_LOS.py
@@ -1,44 +1,44 @@
 # -*- coding: utf-8 -*-
 
-from   __future__           import division
+from __future__ import division
 import h5py
-import numpy                as     np
-from   scipy.optimize       import curve_fit
+import numpy as np
+from scipy.optimize import curve_fit
 import matplotlib
-#matplotlib.use('TkAgg') # <-- THIS MAKES IT FAST!
-import matplotlib.pyplot    as     plt
+# matplotlib.use('TkAgg')  # <-- THIS MAKES IT FAST!
+import matplotlib.pyplot as plt
 import IPython
-#from   matplotlib.colors    import LogNorm
+# from matplotlib.colors import LogNorm
 import time
-from   pylab                import *
 import pylab
-from   scipy.stats          import norm
-#import matplotlib.mlab      as     mlab
-import scipy                as     sp
-from   astropy.io           import ascii
-from   mpl_toolkits.mplot3d import Axes3D
-import seaborn              as     sns
+from scipy.stats import norm
+# import matplotlib.mlab as mlab
+import scipy as sp
+from astropy.io import ascii
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+from pathlib import Path
 
-User_path        =                                    '/Users/gustav.c.rasmussen/'
-Desktop_path     = User_path                        + 'Desktop/'
-GADGET_G_path    = Desktop_path                     + 'RunGadget/G_perturbations/'
-Stable_path      =                                    'LOS/Stable_structures/'
-figure_path      = Desktop_path + Stable_path       + 'figures/'
+User_path = Path.cwd() + '/Users/gustav.c.rasmussen/'
+Desktop_path = User_path + 'Desktop/'
+GADGET_G_path = Desktop_path + 'RunGadget/G_perturbations/'
+Stable_path = 'LOS/Stable_structures/'
+figure_path = Desktop_path + Stable_path + 'figures/'
 
-#text_files_path  = Desktop_path + Stable_path      + 'text_files/A/'
-#text_files_path  = Desktop_path + Stable_path      + 'text_files/B/'
-#text_files_path  = Desktop_path + Stable_path      + 'text_files/CS4/'
-#text_files_path  = Desktop_path + Stable_path      + 'text_files/CS5/'
-#text_files_path  = Desktop_path + Stable_path      + 'text_files/CS6/'
-#text_files_path  = Desktop_path + Stable_path      + 'text_files/DS1/'
-#text_files_path  = Desktop_path + Stable_path      + 'text_files/Soft_D2/'
-#text_files_path  = Desktop_path + Stable_path      + 'text_files/E/'
+# text_files_path = Desktop_path + Stable_path + 'text_files/A/'
+# text_files_path = Desktop_path + Stable_path + 'text_files/B/'
+# text_files_path = Desktop_path + Stable_path + 'text_files/CS4/'
+# text_files_path = Desktop_path + Stable_path + 'text_files/CS5/'
+# text_files_path = Desktop_path + Stable_path + 'text_files/CS6/'
+# text_files_path = Desktop_path + Stable_path + 'text_files/DS1/'
+# text_files_path = Desktop_path + Stable_path + 'text_files/Soft_D2/'
+# text_files_path = Desktop_path + Stable_path + 'text_files/E/'
 
-Martin_path      =                                    'Martin_IC_and_Final_Edd_and_OM/'
-hdf5_path        = Desktop_path                     + 'G_perturbations/hdf5_files/'
-nosync_path      = User_path                        + 'nosync/RunGadget/'
+Martin_path = 'Martin_IC_and_Final_Edd_and_OM/'
+hdf5_path = Desktop_path + 'G_perturbations/hdf5_files/'
+nosync_path = User_path + 'nosync/RunGadget/'
 
-# Filename       = hdf5_path                        + '0G00_IC_000.hdf5'
+# Filename = hdf5_path                        + '0G00_IC_000.hdf5'
 # Filename       = hdf5_path                        + '0G20_Final_000.hdf5'
 # Filename       = hdf5_path                        + 'OMG00_001_IC_000.hdf5'
 # Filename       = hdf5_path                        + 'OMG20_Final_000.hdf5'
@@ -174,26 +174,26 @@ new_R_middle     = 0
 if keep_IC_R_middle:
     if F.startswith('Hernquist10000_G'):
         if   Gamma == -1.5:     # logr = -0.70, r = 10**-0.70  
-                R_middle = 10**-0.70 
+                R_middle = 10**-.70 
         elif Gamma == -2.0:     # logr = -0.25, r = 10**-0.25
-                R_middle = 10**-0.25
+                R_middle = 10**-.25
         elif Gamma == -2.5:     # logr = 0.0 , r = 10**0.0               
-                R_middle = 10**-0.0 
+                R_middle = 10**-.0 
         elif Gamma == -3.0:     # logr = 0.30 , r = 10**0.30
-                R_middle = 10**-0.30
+                R_middle = 10**-.30
     if F.startswith('OsipkovMerritt_'):
-        if   Gamma == -1.5:     # logr =  
+        if   Gamma == -1.5:
             R_middle =  0
-        elif Gamma == -2.0:     # logr = 
+        elif Gamma == -2.0:
             R_middle = 0
-        elif Gamma == -2.5:     # logr =               
+        elif Gamma == -2.5:
             R_middle = 0
-        elif Gamma == -3.0:     # logr = 
+        elif Gamma == -3.0:
             R_middle = 0
 
-if new_R_middle:# Choose new R_middle for each file.
-    if F == 'Hernquist10000_G1.0_0_000': # 0.th/IC file
-        if   Gamma == -1.5:     # logr = -0.70, r = 10**-0.70  
+if new_R_middle:  # Choose new R_middle for each file.
+    if F == 'Hernquist10000_G1.0_0_000':  # 0.th/IC file
+        if Gamma == -1.5:     # logr = -0.70, r = 10**-0.70  
             R_middle = 10**-0.70 
         elif Gamma == -2.0:     # logr = -0.25, r = 10**-0.25
             R_middle = 10**-0.25
@@ -202,7 +202,7 @@ if new_R_middle:# Choose new R_middle for each file.
         elif Gamma == -3.0:     # logr = 0.30 , r = 10**0.30
             R_middle = 10**-0.30
     if F == 'Hernquist10000_G1.2_1_005': # 1.st file
-        if   Gamma == -1.5:     # logr =  
+        if Gamma == -1.5:     # logr =  
             R_middle =  10**-0.55
         elif Gamma == -2.0:     # logr = 
             R_middle = 10**-0.4
@@ -290,11 +290,11 @@ R     = ((x-xC)**2+(y-yC)**2+(z-zC)**2)**0.5
 a = 0 # makes sure the while loop is entered.
 x0 = x
 while len(x0)<10000 or a==0:
-    R_limit_min = R_limit_min - 0.000005
-    R_limit_max = R_limit_max + 0.000005
+    R_limit_min -= .000005
+    R_limit_max += .000005
     a += 1
-    GoodIDs = np.where((R<R_limit_max)*(R>R_limit_min))
-    x0 =  x[GoodIDs[0]]
+    GoodIDs = np.where((R < R_limit_max) * (R > R_limit_min))
+    x0 = x[GoodIDs[0]]
 
 x =  x[GoodIDs]
 y =  y[GoodIDs]
@@ -302,13 +302,13 @@ z =  z[GoodIDs]
 vx = vx[GoodIDs]
 vy = vy[GoodIDs]
 vz = vz[GoodIDs] 
-vx = vx - np.median(vx)
-vy = vy - np.median(vy)
-vz = vz - np.median(vz)
+vx -= np.median(vx)
+vy -= np.median(vy)
+vz -= np.median(vz)
 '''
-vx = vx - np.median(vx)
-vy = vy - np.median(vy)
-vz = vz - np.median(vz)
+vx -= np.median(vx)
+vy -= np.median(vy)
+vz -= np.median(vz)
 
 nr_binning_bins = 102
 
@@ -317,8 +317,8 @@ Fig1_xy                           = 0
 Fig2_xz                           = 0
 Fig3_3D_xyz                       = 0
 
-vsphericalnew                     = 1
-vsphericalnew_sigma               = 1
+vsphericalnew                     = 0
+vsphericalnew_sigma               = 0
 
 x14_25_36_same_length             = 0
 save_r_v_as_txt                   = 0
@@ -332,15 +332,15 @@ Fig17_LOS_radius200               = 0
 if Fig1_xy:
     figure()
     plt.subplot(121)
-    plt.plot(x,y)
-    plt.xlabel(r'$x$',fontsize=30)
-    plt.ylabel(r'$y$',fontsize=30)
-    plt.title(r'Positions. $N=%i$, $\gamma = %.2f$ ,  File = %s' %(len(x), Gamma, F),fontsize=30)
+    plt.plot(x, y)
+    plt.xlabel(r'$x$', fontsize=30)
+    plt.ylabel(r'$y$', fontsize=30)
+    plt.title(r'Positions. $N=%i$, $\gamma = %.2f$ ,  File = %s' % (len(x), Gamma, F), fontsize=30)
 
     plt.subplot(122)
-    plt.plot (x,z)
-    plt.xlabel(r'$x$',fontsize=30)
-    plt.ylabel(r'$z$',fontsize=30)
+    plt.plot (x, z)
+    plt.xlabel(r'$x$', fontsize=30)
+    plt.ylabel(r'$z$',f ontsize=30)
 
 if Fig2_xz:
     figure()
@@ -378,62 +378,60 @@ if vsphericalnew: # radial and tangential velocities
     VPhi   = - sp.sin(Phi) * vx + sp.cos(Phi) * vy
     VT     = VTheta + VPhi
 
+
 def func_1(x, a, b):
     return a * x*np.exp(-b * x**2.)
+
+
 def func_2(x, a, b):
     return a * np.exp(-b * x**2.)
+
+
 def func_3(x, a, b):
     return a * x**2*np.exp(-b * x**2.)
+
+
 def func_1_log(log10x, a, b):
     x=10.0**log10x
     return a * x * np.exp(-b * x**2.)
+
+
 def func_2_log(log10x, a, b):
     x=10.0**log10x
     return a * np.exp(-b * x**2.)
+
+
 def func_3_log(log10x, a, b):
     x=10.0**log10x
     return a * x**2 * np.exp(-b * x**2.)
+
+
 def func_4(x, a, q, b):
     return a *(1.- (1. - q )*b*x**2.) **(q/( 1. - q))
+
+
 def func_5(x, b, q):
     return (1.- (1. - q )*b*x**2.) **(q/( 1. - q))
+
+
 def func_4_log(log10x, a, q, b):
     x=10.0**log10x
     return a * x *(1.- (1. - q )*b*x**2.) **(q/( 1. - q))
 
+
 # R_hob_par = R[GoodIDs]
-# v2        = vx**2+vy**2+vz**2
+# v2 = vx**2+vy**2+vz**2
 
-sigma2            = []
-sigmarad2         = []
-sigmatheta2       = []
-sigmaphi2         = []
-sigmatan2         = []
-sigma             = []
-sigmarad          = []
-sigmatheta        = []
-sigmaphi          = []
-sigmatan          = []
-VR_sigmarad       = []
-VTheta_sigmatheta = []
-VPhi_sigmaphi     = []
-VT_sigmatan       = []
-r                 = []
-Phi               = []
-Theta             = []
-VR                = []
-VTheta            = []
-VPhi              = []
-VT                = []
-R_LOS_arr         = []
-vx_LOS_arr        = []
+(sigma2, sigmarad2, sigmatheta2, sigmaphi2, sigmatan2, sigma, sigmarad,
+ sigmatheta, sigmaphi, sigmatan, VR_sigmarad, VTheta_sigmatheta,
+ VPhi_sigmaphi, VT_sigmatan, r, Phi, Theta, VR, VTheta, VPhi, VT,
+ R_LOS_arr, vx_LOS_arr, bin_radius_arr) = ([] for i in range(24))
+
 '''
-binning_arr       = np.linspace(R_limit_min,R_limit_max,nr_binning_bins) 
-bin_radius_arr    = []
+binning_arr = np.linspace(R_limit_min, R_limit_max, nr_binning_bins) 
 
-for i in range(nr_binning_bins-2):
-#for i in range(nr_binning_bins):    
-    min_R_bin_i            = binning_arr[i]    # start of bin
+for i in range(nr_binning_bins-2): 
+    min_R_bin_i            = binning_arr[i]  # start of bin
     max_R_bin_i            = binning_arr[i+1]  # end of bin
     posR_par_inside_bin_i  = np.where((R_hob_par>min_R_bin_i) & (R_hob_par<max_R_bin_i)) # position of particles inside a radial bin
     nr_par_inside_bin_i    = len(posR_par_inside_bin_i[0]) 
@@ -544,21 +542,14 @@ VPhi_sigmaphi         = np.concatenate(np.array(VPhi_sigmaphi),axis = 0)
 VT_sigmatan           = np.concatenate(np.array(VT_sigmatan),axis = 0)
 
 '''
-v_rp = []    # divide into 6 graphs
-v_rn = []
-v_thetap = []
-v_thetan = []
-v_phip = []
-v_phin = []
-v_tp = []
-v_tn = []
+
+# divide into 6 graphs
+(v_rp, v_rn, v_thetap, v_thetan, v_phip, v_phin, v_tp, v_tn) = ([] for i in range(8))
 
 if vsphericalnew:
     for i in range(len(VR)):
-        if VR[i] >= 0.:
-            v_rp.append(VR[i])
-        else:
-            v_rn.append(VR[i])
+        v_rp.append(VR[i]) if VR[i] >= 0. else v_rn.append(VR[i])
+
     v_rp_arr = np.asarray(v_rp)
     v_rn_arr = np.asarray(v_rn)
 
@@ -622,14 +613,8 @@ if x14_25_36_same_length:
     x6 = np.asarray(x6)                           
 
 if vsphericalnew_sigma:
-    VR_sigmarad_p       = []
-    VR_sigmarad_n       = []
-    VTheta_sigmatheta_p = []
-    VTheta_sigmatheta_n = []
-    VPhi_sigmaphi_p     = []
-    VPhi_sigmaphi_n     = []
-    VT_sigmatan_p       = []
-    VT_sigmatan_n       = []
+    (VR_sigmarad_p, VR_sigmarad_n, VTheta_sigmatheta_p, VTheta_sigmatheta_n,
+    VPhi_sigmaphi_p, VPhi_sigmaphi_n, VT_sigmatan_p, VT_sigmatan_n) = ([] for i in range(8))
 
     for i in range(len(VR_sigmarad)):
         if VR_sigmarad[i] >= 0.:
@@ -683,19 +668,19 @@ if Fig13_LOS:
     ax2.set_ylabel(r'Line-of-sight velocity, $v_x$', fontsize=30)
     ax2.set_title('LOS (A)', fontsize=30)
 
-    #f.savefig(figure_path + 'A_v.png'      )
-    #f.savefig(figure_path + 'B_v.png'      )
-    #f.savefig(figure_path + 'Soft_B_v.png' )
-    #f.savefig(figure_path + 'CS1_v.png'    )
-    #f.savefig(figure_path + 'CS2_v.png'    )
-    #f.savefig(figure_path + 'CS3_v.png'    )
-    #f.savefig(figure_path + 'CS4_v.png'    )
-    #f.savefig(figure_path + 'CS5_v.png'    )
-    #f.savefig(figure_path + 'CS6_v.png'    )
-    #f.savefig(figure_path + 'DS1_v.png'    )
-    #f.savefig(figure_path + 'D2_v.png'     )
-    #f.savefig(figure_path + 'Soft_D2_v.png')
-    #f.savefig(figure_path + 'E_v.png'      )
+    # f.savefig(figure_path + 'A_v.png'      )
+    # f.savefig(figure_path + 'B_v.png'      )
+    # f.savefig(figure_path + 'Soft_B_v.png' )
+    # f.savefig(figure_path + 'CS1_v.png'    )
+    # f.savefig(figure_path + 'CS2_v.png'    )
+    # f.savefig(figure_path + 'CS3_v.png'    )
+    # f.savefig(figure_path + 'CS4_v.png'    )
+    # f.savefig(figure_path + 'CS5_v.png'    )
+    # f.savefig(figure_path + 'CS6_v.png'    )
+    # f.savefig(figure_path + 'DS1_v.png'    )
+    # f.savefig(figure_path + 'D2_v.png'     )
+    # f.savefig(figure_path + 'Soft_D2_v.png')
+    # f.savefig(figure_path + 'E_v.png'      )
 
 if Fig14_LOS_log:
     r           = (x**2+y**2+z**2)**.5
@@ -715,19 +700,19 @@ if Fig14_LOS_log:
     ax2.set_ylabel(r'Line-of-sight velocity,$v_x$',fontsize=30)
     ax2.set_title('LOS (A)',fontsize=30)
 
-    #f.savefig(figure_path + 'A_v.png'      )
-    #f.savefig(figure_path + 'B_v.png'      )
-    #f.savefig(figure_path + 'Soft_B_v.png' )
-    #f.savefig(figure_path + 'CS1_v.png'    )
-    #f.savefig(figure_path + 'CS2_v.png'    )
-    #f.savefig(figure_path + 'CS3_v.png'    )
-    #f.savefig(figure_path + 'CS4_v.png'    )
-    #f.savefig(figure_path + 'CS5_v.png'    )
-    #f.savefig(figure_path + 'CS6_v.png'    )
-    #f.savefig(figure_path + 'DS1_v.png'    )
-    #f.savefig(figure_path + 'D2_v.png'     )
-    #f.savefig(figure_path + 'Soft_D2_v.png')
-    #f.savefig(figure_path + 'E_v.png'      )
+    # f.savefig(figure_path + 'A_v.png'      )
+    # f.savefig(figure_path + 'B_v.png'      )
+    # f.savefig(figure_path + 'Soft_B_v.png' )
+    # f.savefig(figure_path + 'CS1_v.png'    )
+    # f.savefig(figure_path + 'CS2_v.png'    )
+    # f.savefig(figure_path + 'CS3_v.png'    )
+    # f.savefig(figure_path + 'CS4_v.png'    )
+    # f.savefig(figure_path + 'CS5_v.png'    )
+    # f.savefig(figure_path + 'CS6_v.png'    )
+    # f.savefig(figure_path + 'DS1_v.png'    )
+    # f.savefig(figure_path + 'D2_v.png'     )
+    # f.savefig(figure_path + 'Soft_D2_v.png')
+    # f.savefig(figure_path + 'E_v.png')
 
 if Fig15_LOS_radius10:
     r     = (x**2+y**2+z**2)**.5
@@ -850,44 +835,5 @@ if Fig17_LOS_radius200:
     ax4.set_xlim(0,np.log(200))
     ax4.set_xlabel(r'$\log$ R', fontsize=30)
     ax4.set_ylabel(r'Line-of-sight velocity, $v_x$', fontsize=30)
-
-
-
-
-
-
-
-
-
     
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
