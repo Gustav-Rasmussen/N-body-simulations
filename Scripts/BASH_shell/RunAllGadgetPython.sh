ee9228cc70d84d45740b0f2dd51b710957dfaf95
Gustav Rasmussen <gcr84@hotmail.com>
2019-08-28 19:27:53 +0200

    Add script

---
+++ Scripts/BASH_shell/RunAllGadgetPython.sh
@@ -0,0 +1,42 @@
+mpirun -np 4 ./Gadget2 Run0.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run1.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run2.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run3.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run4.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run5.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run6.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run7.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run8.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run9.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run10.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run11.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run12.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run13.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run14.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run15.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run16.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run17.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run18.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run19.param
+python Energy_exchange_D1.py
+mpirun -np 4 ./Gadget2 Run20.param
+python Energy_exchange_D1.py
