0bbed1436ab10b3077bf779a3118d5d7548d8c33
Gustav Rasmussen <gcr84@hotmail.com>
2019-08-31 09:23:44 +0200

    Update script

--- Scripts/BASH_shell/RunAllGadgetPython.sh
+++ Scripts/BASH_shell/RunAllGadgetPython.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+echo "Starting GADGET simulation of energy exchange!"
+
 for i in {0..20}
 do
-   echo "Run number: $i"
+   echo "Starting GADGET run number: $i"
    eval "mpirun -np 4 ./Gadget2 Run$i.param"
+   echo "Analyzing GADGET run number: $i"
    python Energy_exchange_D1.py
 done
+
+echo "GADGET simulation of energy exchange complete!"
